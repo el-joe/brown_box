2cade8abdf832805c0cb566d82ebd10a42e3bf1a
joe <eljoe1717@gmail.com>
2026-08-16 21:03:52 +0300

    Fix

--- laravel/resources/views/components/admin/table.blade.php
+++ laravel/resources/views/components/admin/table.blade.php
@@ -9,6 +9,18 @@
                 serverSide: true,
                 ajax: @js($ajaxUrl),
                 columns: @js($columns),
+                language: {
+                    processing: '',
+                    search: '',
+                    searchPlaceholder: @js(__('Search…')),
+                    lengthMenu: '_MENU_',
+                    info: @js(__('Showing _START_ to _END_ of _TOTAL_ entries')),
+                    infoEmpty: @js(__('No entries found')),
+                    infoFiltered: @js(__('(filtered from _MAX_ total entries)')),
+                    zeroRecords: @js(__('No matching records found')),
+                    paginate: { previous: '<i class=\'fa-solid fa-chevron-left\'></i>', next: '<i class=\'fa-solid fa-chevron-right\'></i>' },
+                },
+                dom: '<\'admin-table-toolbar\'<\'admin-table-length\'l><\'admin-table-search\'f>>rt<\'admin-table-footer\'<\'admin-table-info\'i><\'admin-table-pagination\'p>>',
             });
         },
     }"
@@ -19,15 +31,17 @@
         </div>
     @endisset
 
-    <div class="bg-white rounded-xl border border-slate-200 shadow-sm overflow-x-auto">
-        <table x-ref="table" id="{{ $id }}" class="table table-bordered w-full text-sm">
-            <thead>
-                <tr>
-                    @foreach ($columns as $column)
-                        <th>{{ $column['title'] ?? $column['data'] }}</th>
-                    @endforeach
-                </tr>
-            </thead>
-        </table>
+    <div class="admin-table-card bg-white rounded-xl border border-slate-200 shadow-sm overflow-hidden">
+        <div class="overflow-x-auto">
+            <table x-ref="table" id="{{ $id }}" class="admin-table table w-full text-sm">
+                <thead>
+                    <tr>
+                        @foreach ($columns as $column)
+                            <th>{{ $column['title'] ?? $column['data'] }}</th>
+                        @endforeach
+                    </tr>
+                </thead>
+            </table>
+        </div>
     </div>
 </div>
